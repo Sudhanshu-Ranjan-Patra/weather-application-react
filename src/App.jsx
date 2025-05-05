31c70cebaaedc7a7fc3eacfb80b1e978751d2b8c
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-05-05 16:54:32 +0530

    Geolocation Added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,26 @@ const App = () => {
   const[loading, setLoading] = useState(false);
   const[error, setError] = useState(null);
 
+  // Detect Users Location using Geolocation
+  const getCityFromCoords = async () => {
+    try {
+      const res = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse`, {
+        params: {
+          lat,
+          lon,
+          limit: 1,
+          appid: 'e23358e3c4c2969a6e698af7623ef645',
+        }
+      });
+      if(res.data && res.data.length > 0){
+        return res.data[0].name;
+      }
+    } catch (err) {
+      console.error("Reverse geocoding failed", err);
+    }
+    return null;
+  };
+
   // Fetch weather data
   const fetchWeather = async () => {
     if(!city.trim()) return;
@@ -34,13 +54,41 @@ const App = () => {
     }
   };
 
+  // Background change based on weather
+  const getBackgroundClass = (weatherMain) => {
+    switch(weatherMain){
+      case 'Clear':return 'bg-blue-400';
+      case 'Clouds': return 'bg-gray-400';
+      case 'Rain': return 'bg-blue-700';
+      case 'Thunderstorm': return 'bg-purple-800';
+      case 'Snow': return 'bg-white';
+      case 'Mist': return 'bg-gray-300';
+      default: return 'bg-slate-600';
+    }
+  };
+
   // Fetch weather data on initial load and whenever the city changes
   useEffect(() => {
-    fetchWeather();
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const detectedCity = await getCityFromCoords(latitude,longitude);
+      if(detectedCity){
+        setCity(detectedCity);
+      }
+    },(error) => {
+      console.warn("Geolocation permission denied, using default city.");
+      fetchWeather();
+    });
   }, []);
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      fetchWeather();
+    }
+  };
+
   return (
-    <div className="p-8 bg-blue-800 min-h-screen flex flex-col items-center justify-center">
+    <div className={`p-8 min-h-screen flex flex-col items-center justify-center transition-colors duration-1000 ${getBackgroundClass(weatherData?.weather[0]?.main)}`}>
       <h1 className="text-3xl font-semibold mb-6">
         Weather Application 🌤️
       </h1>
@@ -63,12 +111,26 @@ const App = () => {
         </button>
     </div>
       {/* Display weather or loading/error message */}
-      {loading && <p>Loading...</p>}
+      {loading && (
+        <div className="flex items-center justify-center space-x-2 text-blue-400">
+          <svg className="animate-spin h-6 w-6 text-blue-400 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+          </svg>
+          <span>Fetching weather data...</span>
+        </div>
+      )}
       {error && <p className="text-red-500">{error}</p>}
 
+      {/*Displaying Fetched data*/}
       {weatherData && !loading && !error &&(
         <div className="bg-white p-6 rounded-lg shadow-md text-center w-72">
           <h2 className="text-2xl font-semibold mb-2">{weatherData.name}</h2>
+          <img 
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
+            alt="weather-icon" 
+            className="w-20 h-20 mx-auto mb-2"
+          />
           <p className="capitalize text-gray-600 mb-1">{weatherData.weather[0].description}</p>
           <p className="text-lg">🌡️ {weatherData.main.temp}°C</p>
           <p className="text-sm text-gray-500">
